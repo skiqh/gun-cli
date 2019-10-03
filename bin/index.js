0186eb2222eb51f7fcca10c17a2e7d0aebf6e8d4
skiqh <julian@urknall.co>
2019-10-03 21:52:21 +0200

    watch a gun path

--- bin/index.js
+++ bin/index.js
@@ -15,6 +15,7 @@ const minimist_options =
 const config = minimist(process.argv.slice(2), minimist_options)
 
 const Gun = require('gun')
+const gun_path = require('gun/lib/path')
 const server = require('http').createServer().listen(config.port, config.host)
 const gun = Gun({...config, web: server})
 
@@ -25,10 +26,23 @@ monitorctrlc.monitorCtrlC(() => {
 })
 
 
-// use setTimeout to print after the output on Gun()
-setTimeout(()=> {
+function watch(pth) {
+	if(!pth)
+		return
+	console.log(`watching ${colors.yellow(pth)} ...`)
+	gun.path(pth).on(data => {
+		console.log(`${(new Date()).toLocaleTimeString()}\t${colors.yellow(pth)} =>`, colors.brightCyan(JSON.stringify(data, null, '\t')))
+	})
+}
+
+function afterinit() {
 	const server_url = colors.brightBlue.underline(`http://${config.host}:${config.port}/gun`)
 	console.log()
 	console.log(`Gun node running at ${server_url}`)
-	console.log(`To stop the server, press Ctrl+C`)
-})
+	console.log(`To stop it, press Ctrl+C`)
+	console.log()
+	watch(config.watch)
+}
+
+// use setTimeout to print after the output on Gun()
+setTimeout(afterinit)
