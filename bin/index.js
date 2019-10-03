206fe351e222486e6a88dbbe8f172ede427ecc79
skiqh <julian@urknall.co>
2019-10-03 17:17:50 +0200

    spin up a gun instance with default params

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,17 @@
 #!/usr/bin/env node
 
-console.log('running gun-cli', process.argv.slice(2))
+const minimist_options =
+	{	default:
+		{	port: 8765
+		,	host: '0.0.0.0'
+		,	file: './db/'
+		,	radisk: true
+		}
+	}
+const config = require('minimist')(process.argv.slice(2), minimist_options)
+
+const Gun = require('gun')
+const server = require('http').createServer().listen(config.port, config.host)
+const gun = Gun({web: server, file: config.file, radisk: config.radisk})
+
+console.log(`connect to your local gun node at http://${config.host}:${config.port}/gun`)
