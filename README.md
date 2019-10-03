05d9dd5138fac8e751928898e16a726c9cc316b7
skiqh <julian@urknall.co>
2019-10-03 22:22:28 +0200

    add comand line options to README

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # gun-cli
 run a gun server from your command line
+
+# usage
+
+	gun [options]
+
+
+## options
+
+	--host STRING            0.0.0.0     set the ip to listen on
+	--port NUMBER            8765        set the port to listen on
+	--file STRING            ./db/       set gun's file parameter
+	--peers STRING,STRING                comma-seperated list of peers' ips
+	--watch PATH                         log changes with gun.path(PATH).on()
+
--- bin/index.js
+++ bin/index.js
@@ -9,10 +9,12 @@ const minimist_options =
 		{	port: 8765
 		,	host: '0.0.0.0'
 		,	file: './db/'
-		,	radisk: true
 		}
 	}
 const config = minimist(process.argv.slice(2), minimist_options)
+
+if(config.peers)
+	config.peers = config.peers.split(',')
 
 const Gun = require('gun')
 const gun_path = require('gun/lib/path')
@@ -21,7 +23,7 @@ const gun = Gun({...config, web: server})
 
 
 monitorctrlc.monitorCtrlC(() => {
-	console.log(`bye.`)
+	console.log(colors.gray(`bye.`))
 	process.exit()
 })
 
@@ -29,7 +31,7 @@ monitorctrlc.monitorCtrlC(() => {
 function watch(pth) {
 	if(!pth)
 		return
-	console.log(`watching ${colors.yellow(pth)} ...`)
+	console.log(colors.gray(`Watch: ${colors.yellow(pth)}`))
 	gun.path(pth).on(data => {
 		console.log(`${(new Date()).toLocaleTimeString()}\t${colors.yellow(pth)} =>`, colors.brightCyan(JSON.stringify(data, null, '\t')))
 	})
@@ -37,11 +39,16 @@ function watch(pth) {
 
 function afterinit() {
 	const server_url = colors.brightBlue.underline(`http://${config.host}:${config.port}/gun`)
+	const peers = config.peers && config.peers.map(p => colors.yellow(p)).join(', ')
 	console.log()
 	console.log(`Gun node running at ${server_url}`)
-	console.log(`To stop it, press Ctrl+C`)
 	console.log()
+	console.log(colors.gray(`File:  ${colors.yellow(config.file)}`))
+	if(peers)
+		console.log(colors.gray(`Peers: ${peers}`))
 	watch(config.watch)
+	console.log()
+	console.log(colors.brightRed(`To stop, press Ctrl+C`))
 }
 
 // use setTimeout to print after the output on Gun()
