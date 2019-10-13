7a5e0946e52309a7503b5162c40a66d734342264
skiqh <julian@urknall.co>
2019-10-13 18:51:13 +0200

    pass peers as an object to the Gun constructor

--- bin/index.js
+++ bin/index.js
@@ -9,12 +9,13 @@ print PATH                           load NODEPATH and print as JSON
 
 GENERAL OPTIONS
 --no-color                           don't use any colors in output
+--debug                              print GUN debug info
 
 [serve] OPTIONS
 --host STRING            0.0.0.0     set the ip to listen on
 --port NUMBER            8765        set the port to listen on
 --file STRING            ./db/       set gun's file parameter
---peers STRING,STRING                comma-seperated list of peers' ips
+--peers STRING,STRING                comma-seperated list of peers
 --watch PATH                         log changes with gun.path(PATH).on()
 
 [print] OPTIONS
@@ -35,10 +36,6 @@ const minimist_options =
 		}
 	}
 const config = minimist(process.argv.slice(2), minimist_options)
-
-if(config.peers)
-	config.peers = config.peers.split(',')
-
 
 monitorctrlc.monitorCtrlC(() => {
 	console.log(colors.gray(`bye.`))
--- bin/serve.js
+++ bin/serve.js
@@ -4,6 +4,12 @@ module.exports = function cmd_serve(config, cb) {
 	if(config.debug)
 		process.env.GUN_ENV = 'debug'
 
+	if(config.peers)
+		config.peers = config.peers.split(',').reduce((acc,peer) => {
+			acc[peer] = {}
+			return acc
+		}, {})
+
 	const Gun = require('gun')
 	const gun_path = require('gun/lib/path')
 	const server = require('http').createServer().listen(config.port, config.host)
@@ -24,7 +30,7 @@ module.exports = function cmd_serve(config, cb) {
 
 	function afterinit() {
 		const server_url = colors.brightBlue.underline(`http://${config.host}:${config.port}/gun`)
-		const peers = config.peers && config.peers.map(p => colors.yellow(p)).join(', ')
+		const peers = config.peers && Object.keys(config.peers).map(p => colors.yellow(p)).join(', ')
 		console.log()
 		console.log(`Gun node running at ${server_url}`)
 		console.log()
