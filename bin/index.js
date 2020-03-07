7d299e1f066b8f8465c48afc08f5409bd2cb4ecd
skiqh <julian@urknall.co>
2020-03-07 17:45:10 +0100

    add some command line parameters; auto-discover certs from ./certs

--- bin/index.js
+++ bin/index.js
@@ -13,14 +13,19 @@ GENERAL OPTIONS
                                 (IPs are expanded to http://IP:8765/gun)
 --no-color                      do not use any colors in output
 --debug                         print GUN debug info
+--silent                        reduce command line output
+--repl                          go into a repl (with gun instace)
 
 [serve] OPTIONS
 --host STRING       0.0.0.0     set the ip to listen on
 --port NUMBER       8765        set the port to listen on
 --watch PATH                    log changes with gun.path(PATH).on()
---certs PATH                    use https with cert files from PATH
-                                (privkey.pem, fullchain.pem, chain.pem)
+--certs PATH        ./certs     use https with cert files from PATH
+                                (key.pem, cert.pem, ca.pem)
+--nocerts                       disable auto-discovery of ./certs
 --webrtc                        load lib/webrtc
+--axe                           use axe
+--multicast                     use multicast
 
 [print] OPTIONS
 --out FILENAME                  write to FILENAME instead of stdout
@@ -30,75 +35,69 @@ GENERAL OPTIONS
 --timeout NUMBER    1000        wait this much for answers to your request
 `
 
-const minimist = require('minimist')
-const monitorctrlc = require('monitorctrlc')
-const colors = require('colors/safe')
-const net = require('net')
+const minimist = require("minimist")
+const monitorctrlc = require("monitorctrlc")
+const colors = require("colors/safe")
+const net = require("net")
 
-
-const minimist_options =
-	{	default:
-		{	port: 8765
-		,	host: '0.0.0.0'
-		,	file: './gundata/'
-		}
+const minimist_options = {
+	default: {
+		port: 8765,
+		host: "0.0.0.0",
+		file: "./gundata/",
+		repl: false,
+		webrtc: false,
+		axe: false,
+		multicast: false
 	}
+}
 const config = minimist(process.argv.slice(2), minimist_options)
 
-if(config.peers) {
+if (config.peers) {
 	config.peers = config.peers
-		.split(',')
-		.map(peer => net.isIP(peer) ? `http://${peer}:8765/gun`:peer )
-		.reduce((acc,peer) => {
+		.split(",")
+		.map(peer => (net.isIP(peer) ? `http://${peer}:8765/gun` : peer))
+		.reduce((acc, peer) => {
 			acc[peer] = {}
 			return acc
 		}, {})
 }
 
-monitorctrlc.monitorCtrlC(() => {
-	console.log(colors.gray(`bye.`))
-	process.exit()
-})
-
-
-
-const command_name = config._[0] || 'serve'
-if(command_name === 'help' || !!config.help)
-	return command_cb(0, null, true)
-if(command_name === 'version' || !!config.version) {
-	const g = require('gun')
-	const pck = require('../package.json')
+const command_name = config._[0] || "serve"
+if (command_name === "help" || !!config.help) return command_cb(0, null, true)
+if (command_name === "version" || !!config.version) {
+	const g = require("gun")
+	const pck = require("../package.json")
 	return command_cb(0, `gun-cli: ${pck.version}\ngun:     ${g.version}`, false)
 }
 
 try {
 	const command_fn = require(`./${command_name}`)
 	const command_res = command_fn(config, command_cb)
-	if(command_res && !config.norepl) {
-		const repl = require('repl').start('> ')
-		for(key in command_res)
-			repl.context[key] = command_res[key]
+
+	if (command_res) {
+		monitorctrlc.monitorCtrlC(() => {
+			console.log()
+			console.log(colors.gray(`bye.`))
+			process.exit()
+		})
+		if (config.repl) {
+			const repl = require("repl").start("> ")
+			for (key in command_res) repl.context[key] = command_res[key]
+		}
 	}
-}
-catch(ex) {
-	if(ex && ex.code === 'MODULE_NOT_FOUND')
+} catch (ex) {
+	if (ex && ex.code === "MODULE_NOT_FOUND")
 		command_cb(101, `unknown command ${colors.underline(command_name)}`, true)
-	else
-		console.log(`ERROR`, ex)
+	else console.log(`ERROR`, ex)
 }
-
-
 
 function command_cb(err_code, err_string, print_usage) {
-	if(err_string)
-		console.log(colors.brightRed(`\n${err_string}\n`))
-	if(print_usage) {
+	if (err_string) console.log(colors.brightRed(`\n${err_string}\n`))
+	if (print_usage) {
 		console.log(usage)
 	}
-	if(typeof err_code === 'number')
-		process.exit(err_code)
-	else if(err_string)
-		process.exit(1)
-	else
-		console.log(colors.brightRed(`To stop, press Ctrl+C`))
+	if (typeof err_code === "number") process.exit(err_code)
+	else if (err_string) process.exit(1)
+	else console.log(colors.brightRed(`To stop, press Ctrl+C`))
 }
--- bin/serve.js
+++ bin/serve.js
@@ -1,40 +1,44 @@
-const colors = require('colors/safe')
+const colors = require("colors/safe")
+const fs = require("fs")
+const path = require("path")
 
 module.exports = function cmd_serve(config, cb) {
-	if(config.debug)
-		process.env.GUN_ENV = 'debug'
+	if (config.debug) process.env.GUN_ENV = "debug"
 
-	const Gun = require('gun')
-	require('gun/lib/path')
+	const Gun = require("gun")
+	require("gun/lib/path")
 
-	if(config.webrtc) {
+	if (config.webrtc) {
 		console.log(`loading webrtc`)
-		require('gun/lib/webrtc')
+		require("gun/lib/webrtc")
 	}
 
-	const gun_config = {...config}
+	const gun_config = { ...config }
 
-	if(config.certs) {
-		const fs = require('fs')
-		const path = require('path')
+	const found_key = fs.existsSync(path.resolve("./certs/key.pem"))
+	const found_cert = fs.existsSync(path.resolve("./certs/cert.pem"))
+	const found_ca = fs.existsSync(path.resolve("./certs/ca.pem"))
+
+	if (!config.nocerts && found_key && found_cert && found_ca)
+		config.certs = "./certs"
+
+	if (config.certs) {
 		try {
-			const http_config =
-				{	key: fs.readFileSync(path.resolve(config.certs, 'privkey.pem'),)
-				,	cert: fs.readFileSync(path.resolve(config.certs, 'fullchain.pem'),)
-				,	ca: fs.readFileSync(path.resolve(config.certs, 'chain.pem'))
-				}
-			gun_config.web = require('https').createServer(http_config)
-		}
-		catch(https_ex) {
-			if(https_ex.code === 'EACCES')
+			const http_config = {
+				key: fs.readFileSync(path.resolve(config.certs, "key.pem")),
+				cert: fs.readFileSync(path.resolve(config.certs, "cert.pem")),
+				ca: fs.readFileSync(path.resolve(config.certs, "ca.pem"))
+			}
+			gun_config.web = require("https").createServer(http_config)
+		} catch (https_ex) {
+			if (https_ex.code === "EACCES")
 				return cb(301, `not allowed to access certificates`)
 
 			console.log(`https_ex`, https_ex)
-			return cb(310, 'could not start an https server')
+			return cb(310, "could not start an https server")
 		}
-	}
-	else {
-		gun_config.web = require('http').createServer()
+	} else {
+		gun_config.web = require("http").createServer()
 	}
 
 	gun_config.web.listen(config.port, config.host)
@@ -44,34 +48,41 @@ module.exports = function cmd_serve(config, cb) {
 	// gun.on('out', {get: {'#': {'*': ''}}})
 
 	function watch(pth, cb) {
-		if(!pth)
-			return cb()
+		if (!pth) return cb()
 		gun.path(pth).once(_ => {
 			console.log(colors.gray(`Watch: ${colors.yellow(pth)}`))
 
 			cb()
 			gun.path(pth).on(data => {
-				console.log(`${(new Date()).toLocaleTimeString()}\t${colors.yellow(pth)} =>`, colors.brightGreen(JSON.stringify(data, null, '\t')))
+				console.log(
+					`${new Date().toLocaleTimeString()}\t${colors.yellow(pth)} =>`,
+					colors.brightGreen(JSON.stringify(data, null, "\t"))
+				)
 			})
 		})
 	}
 
 	function afterinit() {
-		const server_url = colors.brightBlue.underline(`${config.certs ? 'https':'http'}://${config.host}:${config.port}/gun`)
-		const peers = config.peers && Object.keys(config.peers).map(p => colors.yellow(p)).join(', ')
+		const server_url = colors.brightBlue.underline(
+			`${config.certs ? "https" : "http"}://${config.host}:${config.port}/gun`
+		)
+		const peers =
+			config.peers &&
+			Object.keys(config.peers)
+				.map(p => colors.yellow(p))
+				.join(", ")
 		console.log()
 		console.log(`Gun node running at ${server_url}`)
 		console.log()
 		console.log(colors.gray(`File:  ${colors.yellow(config.file)}`))
 
-		if(config.certs)
+		if (config.certs)
 			console.log(colors.gray(`Certs: ${colors.yellow(config.certs)}`))
-		if(peers)
-			console.log(colors.gray(`Peers: ${peers}`))
+		if (peers) console.log(colors.gray(`Peers: ${peers}`))
 		watch(config.watch, cb)
 	}
 
 	// use setTimeout to print after the output on Gun()
 	setTimeout(afterinit)
-	return {gun}
+	return { gun }
 }
